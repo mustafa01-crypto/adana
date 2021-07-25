82282404479a6cd691f83dcaced09924c12116b8
mustafa01-crypto <hasansabbah284@gmail.com>
2021-07-25 17:31:47 +0300

    morfizm

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 // @dart=2.9
 import 'package:adana/auth/login.dart';
+import 'package:adana/onBoarding/onBoarding.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main()  async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +24,47 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-     home: Login(),
+     home: Splash(),
+    );
+  }
+}
+class Splash extends StatefulWidget {
+@override
+SplashState createState() => new SplashState();
+}
+
+class SplashState extends State<Splash> {
+
+
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => new Login()));
+    } else {
+      await prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => new OnBoardingPage()));
+    }
+  }
+
+  @override
+  void afterFirstLayout(BuildContext context) => checkFirstSeen();
+
+  @override
+  void initState() {
+   checkFirstSeen();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Text('Yükleniyor'),
+      ),
     );
   }
 }
---
+++ lib/onBoarding/onBoarding.dart
@@ -0,0 +1,116 @@
+import 'package:adana/auth/login.dart';
+import 'package:adana/constants/constants.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class OnBoardingPage extends StatefulWidget {
+  @override
+  _OnBoardingPageState createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => Login()),
+    );
+  }
+
+
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('assets/$assetName', width: width);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = const PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: kutu,
+      globalHeader: Align(
+        alignment: Alignment.topRight,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16, right: 16),
+            child: _buildImage('sifir.png', 50),
+          ),
+        ),
+      ),
+      globalFooter: SizedBox(
+        width: double.infinity,
+        height: 60,
+
+        child: ElevatedButton(
+
+          child: const Text(
+            'Hadi Başlayalım',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () => _onIntroEnd(context),
+        ),
+      ),
+      pages: [
+        PageViewModel(
+          title: "Gez Gör Keşfet",
+          body:
+          "Güzide ilimizde bulunan tarihi ve doğal güzellikleri keşfedin",
+          image: _buildImage('merkezcami.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Yorum Yap",
+          body:
+          "Gezdiğiniz yerler hakkında yorum yapın.",
+          image: _buildImage('yorum.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Tek tuşla istediğiniz mekana gidin",
+          body:
+          "Uygulumamızdaki harita özelliği sayesinde uygulamamızda bulunan yerlere tek tıkla gidebilirsiniz.",
+          image: _buildImage('map.jpeg'),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipFlex: 0,
+      nextFlex: 0,
+      //rtl: true, // Display as right-to-left
+      skip: const Text('Geç'),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Bitti', style: TextStyle(fontWeight: FontWeight.w600)),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(12.0)
+          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator:  ShapeDecoration(
+        color: sol,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+    );
+  }
+}

a5c2c6b4a0a1f5f9711e1098e454a117f2c3e42d
mustafa01-crypto <hasansabbah284@gmail.com>
2021-08-12 00:03:36 +0300

    prof

--- lib/ilceler/seyhan/seyhanYorumlar/taskopruYorum.dart
+++ lib/ilceler/seyhan/seyhanYorumlar/taskopruYorum.dart
@@ -1,5 +1,6 @@
 import 'package:adana/constants/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_stars/flutter_rating_stars.dart';
@@ -43,12 +44,13 @@ class Yorumlar extends StatefulWidget {
 }
 
 class _YorumlarState extends State<Yorumlar> {
+  double value = 1.0;
   @override
   Widget build(BuildContext context) {
     Query karapinarYorumlar =
         FirebaseFirestore.instance.collection('TasKopruYorum');
 
-    double value = 1.0;
+
     var now = new DateTime.now();
     var formatter = new DateFormat('dd-MM-yyyy');
     String formattedDate = formatter.format(now);
@@ -79,7 +81,7 @@ class _YorumlarState extends State<Yorumlar> {
                     padding: EdgeInsets.symmetric(
                         horizontal: MediaQuery.of(context).size.width * 1 / 50),
                     decoration: BoxDecoration(
-                    //  border: Border.all(color: scaffold, width: 4),
+                      //  border: Border.all(color: scaffold, width: 4),
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(15),
                           topRight: Radius.circular(15),
@@ -91,8 +93,9 @@ class _YorumlarState extends State<Yorumlar> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
+                            Profiles(path: data['email']),
                             Text(
                               data["email"],
                               style: email,
@@ -155,6 +158,57 @@ class _YorumlarState extends State<Yorumlar> {
           }).toList(),
         );
       },
+    );
+  }
+}
+
+class Profiles extends StatefulWidget {
+  String? path;
+
+  Profiles({required this.path});
+
+  @override
+  _ProfilesState createState() => _ProfilesState();
+}
+
+class _ProfilesState extends State<Profiles> {
+
+  String? indirmeBaglantisi;
+
+  baglantiAl() async {
+    String baglanti = await FirebaseStorage.instance
+        .ref()
+        .child("profilresimleri")
+        .child(widget.path!)
+        .child("profilResmi.png")
+        .getDownloadURL();
+
+    setState(() {
+      indirmeBaglantisi = baglanti;
+    });
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    baglantiAl();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    // final height = MediaQuery.of(context).size.height;
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipOval(
+            child:  Image.network(
+              indirmeBaglantisi!,
+              width: width * 3 / 30,
+              height: width * 2 / 18,
+              fit: BoxFit.cover,
+            )),
+      ),
     );
   }
 }
